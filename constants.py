caa5e9507b4f0bdb819c9b49d19444ef5e074db5
akshtshrma24 <akshitsharmaa65@gmail.com>
2022-08-18 16:38:47 -0700

    initial commit

---
+++ constants.py
@@ -0,0 +1,20 @@
+HEADERS_GET_DATA={
+    'authority': 'phx.unusualwhales.com',
+    'accept': 'application/json, text/plain, */*',
+    'accept-language': 'en-US,en;q=0.9',
+    'authorization': 'Bearer null',
+    'origin': 'https://unusualwhales.com',
+    'referer': 'https://unusualwhales.com/',
+    'sec-ch-ua': '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
+}
+PARAMS={
+    'db': 'mydb',
+}
+HEADERS_INFLUX={'Content-Type': 'application/x-www-form-urlencoded',}
+DATA_QUERY='q=SELECT * FROM "stockHist"'
---
+++ influxHandler.py
@@ -0,0 +1,27 @@
+import json
+import requests
+from datetime import date
+import constants
+import os
+
+def getAndPostData():
+    response = requests.get('https://phx.unusualwhales.com/api/senate_stocks/Nancy%20Pelosi', headers=constants.HEADERS_GET_DATA)
+    for item in response.json()['senate_stocks']:
+        if(str(date.today()) in str(item['filed_at_date'])):
+            data = f"{item['txn_type']} {item['symbol']} {item['filed_at_date']}"
+            if(not isInside(data)):
+                postData(data)
+                
+def isInside(check):          
+    response = requests.post('http://localhost:8086/query', params=constants.PARAMS,\
+     headers=constants.HEADERS_INFLUX, data=constants.DATA_QUERY)
+    if(response.text == '{"results":[{"statement_id":0}]}' or not check in response.text):
+        return False
+    return True
+
+def postData(toSend):
+    headers = {'Content-Type': 'application/x-www-form-urlencoded',}
+    params = {'db': 'mydb',}
+    toSend = f'stockHist,mytag=Nancy fullTransaction="{toSend}"'
+    response = requests.post('http://localhost:8086/write', params=params, headers=headers, data=toSend)
+
